d90cbb3ddc166658aa0583811a2e4b60535e7cb2
David Wadmark <david.wadmark@gmail.com>
2022-06-11 17:58:49 +0200

    Implement view to list tracks

--- togetherlist/app/urls.py
+++ togetherlist/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('tracks', views.list_tracks, name='list tracks')
+    path('tracks', views.list_tracks, name='list tracks'),
+    path('vote', views.vote, name='vote'),
+    path('register-vote', views.register_vote, name='register vote')
 ]
--- togetherlist/app/views.py
+++ togetherlist/app/views.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
+from .forms import NameForm
 from app.apicaller import ApiCaller
 
-PLAYLIST_ID = "3OfKuiS37oW6ciU88lWffV?si=fc3a61cd5aa14b92"
+
+PLAYLIST_ID = "7db8ovaFEAB4blO9f1oEEy?si=c05681fba3d546f6"
 
 
 def index(request):
@@ -14,4 +16,29 @@ def index(request):
 def list_tracks(request):
     request_url = 'playlists/{playlist_id}/tracks'.format(playlist_id=PLAYLIST_ID)
     response = ApiCaller.get(request_url)
-    return JsonResponse(response.json())
+    response_dict = response.json()
+    track_string_list = []
+    for track in response_dict['tracks']['items']:
+        name = track['track']['name']
+        artists = [artist['name'] for artist in track['track']['artists']]
+        track_string_list.append('{artists} - {track}'.format(artists=', '.join(artists), track=name))
+
+    return render(request, 'list.html', {'tracks': track_string_list})
+
+
+def vote(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/register-vote')
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
+
+def register_vote(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('Thank you for voting.')
