d39ea143e9041a4ee83bfc9b9d6b0c9ff4416e6f
David Wadmark <david.wadmark@gmail.com>
2022-06-19 14:36:16 +0200

    Finish initial implementation of login and signup

--- togetherlist/app/views.py
+++ togetherlist/app/views.py
@@ -1,21 +1,17 @@
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.conf import settings
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .forms import TrackForm, UserForm, UserRegistrationForm
-from .models import Track
+from .forms import TrackForm, UserForm
 from app.apicaller import ApiCaller
-
 
 
 PLAYLIST_ID = "7db8ovaFEAB4blO9f1oEEy?si=c05681fba3d546f6"
 # PLAYLIST_ID = '0bWBuhxBO3Ke2FC9Q6AjZk?si=f99503563b884268'
-
-def is_member(user):
-    return user.groups.filter(name='Coronatoppen').exists()
 
 
 def signin(request):
@@ -35,27 +31,19 @@ def signin(request):
         form = UserForm()
         return render(request, 'login.html', {'form': form})
 
+
 def signup(request):
-        if request.method=="POST":
-            first_name = request.POST['first_name']
-            last_name = request.POST['last_name']
-            username = request.POST['username']
-            password = request.POST['password']
-            email = request.POST['email']
-            newuser = User.objects.create_user(
-                first_name=first_name, 
-                last_name=last_name,
-                username=username,
-                password=password,
-                email=email
-            )
-            try:
-                newuser.save()
-            except:
-                return HttpResponse("Something went wrong.")
-        else:
-            form = UserRegistrationForm()
-        return render(request, 'signup.html', {'form':form}) 
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect('/app')
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = UserCreationForm()
+	return render(request=request, template_name="signup.html", context={"form":form})
+
 
 def list_tracks(request):
     request_url = 'playlists/{playlist_id}/tracks'.format(
@@ -73,8 +61,11 @@ def list_tracks(request):
 
 
 @login_required
-@user_passes_test(is_member)
 def vote(request):
+    user = request.user
+    if not user.groups.filter(name='Coronatoppen').exists():
+        messages.info(request, 'An administrator must approve your account.')
+        return redirect('/app/login')
     request_url = 'playlists/{playlist_id}/tracks'.format(
         playlist_id=PLAYLIST_ID)
     response = ApiCaller.get(request_url)
@@ -91,16 +82,13 @@ def vote(request):
     if request.method == 'POST':
         form = TrackForm(request.POST, choices=form_list)
         if form.is_valid():
-            # form.cleaned_data
-            return HttpResponseRedirect('/register-vote')
+            print(form.cleaned_data)
+            return HttpResponseRedirect('register-vote')
     else:
-        form = TrackForm(request.POST, choices=form_list)
-
-    return render(request, 'name.html', {'form': form})
+        form = TrackForm(choices=form_list)
+        return render(request, 'name.html', {'form': form})
 
 
 def register_vote(request):
     if request.method == 'POST':
-        # form = TrackForm(request.POST)
-        # if form.is_valid():
         return HttpResponse('Thank you for voting.')
